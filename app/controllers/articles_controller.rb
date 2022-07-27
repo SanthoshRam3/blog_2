12c7e20961e042ffb0c364064ca30b496c736766
SanthoshRam3 <santhoshram03111998@gmail.com>
2022-07-27 18:01:03 +0530

    Articles Listings with validation messages if we click empty or filled something..

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,18 @@ class ArticlesController < ApplicationController
   end
   def index
     @articles = Article.all
-    #add git
+    
   end
-  
+  def new
+    @article=Article.new
+  end
+  def create
+    @article = Article.new (params.require(:article).permit(:title,:description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+    redirect_to @article
+    else
+      render 'new'
+    end
+  end
 end

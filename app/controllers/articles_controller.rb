d4dec6e4bc7b1fd37d6bf5802ccc9823a2a18590
SanthoshRam3 <santhoshram03111998@gmail.com>
2022-07-28 13:23:29 +0530

    Built a edit and update actions in a existing database tables

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,10 @@ class ArticlesController < ApplicationController
   def new
     @article=Article.new
   end
+  def edit
+    @article=Article.find(params[:id])
+
+  end
   def create
     @article = Article.new (params.require(:article).permit(:title,:description))
     if @article.save
@@ -16,6 +20,15 @@ class ArticlesController < ApplicationController
     redirect_to @article
     else
       render 'new'
+    end
+  end
+  def update
+    @article=Article.find(params[:id])
+    if @article.update (params.require(:article).permit(:title,:description))
+      flash[:notice] = "Article Was Successfully edited"
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
